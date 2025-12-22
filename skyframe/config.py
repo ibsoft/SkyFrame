c26b43ecd1b99b969420dbd7595f934901e08c98
Ioannis A. Bouhras <mycyberdevops@gmail.com>
2025-12-22 11:13:21 +0200

    New Version Release v_5.2.1

--- skyframe/config.py
+++ skyframe/config.py
@@ -56,7 +56,7 @@ class Config:
     CSP_CONNECT_SRC = "'self' https://cdn.jsdelivr.net"
     SHARE_PATH = PROJECT_ROOT / "instance" / "shares"
     ARCHIVE_SUBDIR = "archives"
-    APP_VERSION = os.getenv("APP_VERSION", "v_4.2.10")
+    APP_VERSION = os.getenv("APP_VERSION", "v_5.2.1")
     WATERMARK_OPACITY = int(os.getenv("WATERMARK_OPACITY", "12"))
     WATERMARK_PADDING = int(os.getenv("WATERMARK_PADDING", "12"))
     LOG_DIR = PROJECT_ROOT / "logs"
