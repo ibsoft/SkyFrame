0e1deca9c7574042b12ba963d84a8c79c9cb3582
Ioannis A. Bouhras <mycyberdevops@gmail.com>
2025-12-23 15:01:57 +0200

    minor version bump to v_7.3.4

--- skyframe/config.py
+++ skyframe/config.py
@@ -40,9 +40,9 @@ class Config:
     FEED_SEEN_RETENTION_DAYS = int(os.getenv("FEED_SEEN_RETENTION_DAYS", "30"))
     FEED_SEEN_MAX_IDS = int(os.getenv("FEED_SEEN_MAX_IDS", "5000"))
     FEED_CANDIDATE_MULTIPLIER = int(os.getenv("FEED_CANDIDATE_MULTIPLIER", "3"))
-    FEED_MAX_PER_UPLOADER = int(os.getenv("FEED_MAX_PER_UPLOADER", "3"))
+    FEED_MAX_PER_UPLOADER = int(os.getenv("FEED_MAX_PER_UPLOADER", "20"))
     FEED_MAX_CONSECUTIVE_PER_UPLOADER = int(
-        os.getenv("FEED_MAX_CONSECUTIVE_PER_UPLOADER", "1")
+        os.getenv("FEED_MAX_CONSECUTIVE_PER_UPLOADER", "5")
     )
     MOTD_ENABLED = os.getenv("MOTD_ENABLED", "True").lower() in ("1", "true", "yes")
     VERIFY_MAX_BYTES = int(os.getenv("VERIFY_MAX_BYTES", str(5 * 1024 * 1024)))
